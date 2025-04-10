94c7946692f5b2c78c7f17ab3d9eb26479032073
hopegaju <hopegajupr@gmail.com>
2025-04-10 15:37:17 +0200

    final codes

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class GradeManager {
+    public static void main(String[] args) {
+
+    }
+
+    public void printGrades(int[] grades){
+        for(int grade:grades){
+            System.out.println(grade + " ");
+        }
+    }
+    public void sortGrades(int[] grades){
+        Arrays.sort(grades);
+    }
+    public int  searchGrade(int[] grades, int target){
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int getGrade(int[] grades, int index){
+        try {
+            return grades[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error:Invalid index!");
+            return -1;
+        }
+    }
 }
